3e863549064a652bda3aa8f6a35dbe363e38c2a7
Mex de Loo <mex@zeodev.cc>
2022-03-30 23:32:29 +0200

    add: initial commit

---
+++ Plugin.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Rocket.API.DependencyInjection;
+using Rocket.Core.Logging;
+using Rocket.Core.Plugins;
+
+namespace Cosmo.Unturned;
+
+public class Plugin : Plugin<PluginConfiguration>
+{
+    public Plugin(IDependencyContainer container) : base("Cosmo.Unturned", container)
+    {
+        //
+    }
+
+    protected override async Task OnActivate(bool isFromReload)
+    {
+        Logger.LogInformation("Hello World!");
+        
+        await SaveConfigurationAsync();
+    }
+
+    protected override async Task OnDeactivate()
+    {
+        Logger.LogInformation("Good bye :(");
+    }
+}
---
+++ PluginConfiguration.cs
@@ -0,0 +1,8 @@
+namespace Cosmo.Unturned;
+
+public class PluginConfiguration
+{
+    public string SiteUrl { get; set; }
+    public string ServerToken { get; set; }
+    public int FetchInterval { get; set; }
+}
